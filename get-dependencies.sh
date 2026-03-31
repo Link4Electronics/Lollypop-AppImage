aba26acbb64556076ccb497f85e56c6225e22d30
Link4Electronics <link4electronics@gmail.com>
2026-03-31 15:39:21 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+    lollypop       \
+    pipewire-audio \
+    pipewire-jack  \
+    python
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
